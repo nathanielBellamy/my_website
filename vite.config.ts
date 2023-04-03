a212c653cff7aa361f02a8add71b0f48583a8db1
nathanielBellamy <nbschieber@gmail.com>
2023-04-03 12:29:39 -0700

    remove rollup

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { sveltePreprocess } from 'svelte-preprocess/dist/autoProcess'
-import rust from '@wasm-tool/rollup-plugin-rust';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    rust({ 
-      verbose: true,
-      serverPath: "./src-rust/build/"
-    }),
     svelte({
         preprocess: sveltePreprocess()
     })
