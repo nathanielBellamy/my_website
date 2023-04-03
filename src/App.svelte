7113bd694f5ead4bae6bdfe05724d7d8c3f5a627
nathanielBellamy <nbschieber@gmail.com>
2023-04-02 17:19:40 -0700

    add sass and tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
-
+  let x: number = 1
+  console.dir(x)
 </script>
 
-<main>
-  <div>
+<main class="main rounded-md text-xl">
+  <div class="main_body rounded-md p-5 m-5">
     This is My Website.
   </div>
 </main>
 
-<style>
-
+<style lang="sass">
+  .main
+    background-color: green
+    padding: 50px
+    &_body
+      background-color: blue
 </style>
