5932095db1695855f060426dae94240551824ceb
nathanielBellamy <nbschieber@gmail.com>
2023-04-11 09:01:06 -0700

    aboutme

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,58 @@
 <script lang="ts">
+  import AboutMe from "./lib/AboutMe.svelte"
   import MagicSquare from "./lib/MagicSquare.svelte"
 
   const setCurrentSection = (newSection: string) => {
     currentSection = newSection
   }
 
-  let currentSection: string = "main"
+  let currentSection: string = "aboutMe"
 </script>
 
 <main class="main rounded-md text-xl flex flex-col justify-start">
-  <div class="main_header rounded-md flex flex-row justify-start">
-    <button class="p-5 m-5"
-            on:click={()=>setCurrentSection("main")}>
-      It's My Website
+  <div class="title font-bold text-xl p-7 underline text-left">
+    It's A Website - by Nathan S
+  </div>
+  <div class="main_header flex flex-row gap-x-10 justify-start">
+    <div class="title p-7 text-xl font-bold underline">
+      Things on this Website:
+    </div>
+    <button class="section_button"
+            class:current_section={currentSection == 'aboutMe'}
+            on:click={()=>setCurrentSection("aboutMe")}>
+      About Me
     </button>
-    <button class="p-5 m-5"
-            on:click={()=>setCurrentSection("magicSqaure")}>
+    <button class="section_button"
+            class:current_section={currentSection == 'magicSquare'}
+            on:click={()=>setCurrentSection("magicSquare")}>
       Magic Square
     </button>
   </div>
-  <div class="main_body rounded-md">
-    {#if currentSection == "magicSqaure"}
+  <div class="main_body">
+    {#if currentSection == "magicSquare"}
       <MagicSquare />
     {:else}
       <div>
-        My Website
+        <AboutMe />
       </div>
     {/if}
   </div>
 </main>
 
 <style lang="sass">
+  .title
+    color: black
+  
+  .section_button
+    padding: 0 1em 0 1em
+    margin: .25em
+    font-weight: 700
+  
+  .current_section
+    background-color: lightgrey
+    color: black
+    transition: color .5s, background-color .5s
+    
   .main
     width: 100vw
     height: 100vh
---
+++ src/lib/AboutMe.svelte
@@ -0,0 +1,122 @@
+<script lang="ts">
+  
+  interface PersonalProject {
+    title: string,
+    description: string,
+    href: string
+  }
+  let personal_projects: PersonalProject[] = [
+    {
+      title: 'monthly_budget',
+      description: 'CSV Processing in Rust',
+      href: 'https://github.com/nathanielBellamy/monthly_budget'
+    },
+    {
+      title: 'rustby',
+      description: 'Inject Rust Optimizations into Ruby',
+      href: 'https://github.com/nathanielBellamy/rustby'
+    },
+    {
+      title: 'trow',
+      description: 'Multi-App React Redux Architecture in Typescript',
+      href: 'https://github.com/nathanielBellamy/trow'
+    },
+    {
+      title: 'polynomial_console_graph',
+      description: 'ASCII Graph Polynomials Using C++',
+      href: 'https://github.com/nathanielBellamy/PolynomialConsoleGraph'
+    },
+    {
+      title: 'give_me_a_sine',
+      description: 'ASCII Graph Sinusoidals Using Rust',
+      href: 'https://github.com/nathanielBellamy/give_me_a_sine'
+    }
+  ]
+
+</script>
+
+<div class="about_me flex flex-col justify-start items-stretch">
+  <div class="section">
+    <div class="section_title text-xl font-extrabold">
+      Personal Projects
+    </div>
+    <div class="section_body">
+      {#each personal_projects as { title, description, href } }
+        <div class="project grid grid-cols-4">
+          <a class="project_title"
+               href={href}>
+            {title}
+          </a>
+          <div class="project_description col-span-3">
+            {description}          
+          </div>
+        </div>
+      {/each}
+    </div>
+  </div>
+</div>
+
+<style lang="sass">
+  @use "./../styles/color"
+
+  .about_me
+    width: 100%
+    height: 100%
+
+  .section
+    display: flex
+    justify-content: flex-start
+    align-items: stretch
+    min-height: 200px
+
+    &_title
+      display: flex
+      justify-content: space-around
+      align-items: center
+      background-color: color.$black-4
+      color: color.$white
+      flex-grow: .1
+      font-size: 1.25em
+      padding: 0 5px 0 5px
+    
+    &_body
+      display: flex
+      flex-direction: column
+      justify-content: flex-start
+      align-items: stretch
+      background-color: color.$grey-1
+      color: color.$black-4
+      flex-grow: .9
+
+  .project
+    /* display: flex */
+    /* justify-content: flex-start */
+    align-items: stretch
+    flex-grow: 1
+    border-bottom: 2px solid color.$black-4
+    
+    &_title
+      display: flex
+      justify-content: space-around
+      align-items: center
+      flex-grow: .1
+      background-color: color.$blue-4
+      transition: background-color .25s
+      color: color.$white
+      font-weight: 900
+      padding: 0 5px 0 5px
+      cursor: pointer
+      &:hover
+        background-color: color.$blue-3
+        transition: background-color .25s
+
+    &_description
+      flex-grow: .9
+      text-align: center
+      background-color: color.$yellow-1
+      display: flex
+      align-items: center
+      text-align: left
+      padding-left: 1em
+      font-weight: 700
+</style>
