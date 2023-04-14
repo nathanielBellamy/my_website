48ae19e4beb98987fb40236c9cc7c6564495b7b3
nathanielBellamy <nbschieber@gmail.com>
2023-04-14 08:53:20 -0700

    refactor webgl example

--- src-rust/src/magic_banner/main.rs
+++ src-rust/src/magic_banner/main.rs
@@ -21,58 +21,9 @@ impl MagicBanner {
     // https://rustwasm.github.io/wasm-bindgen/examples/webgl.html
     pub fn run(&self) -> Result<(), JsValue> {
         let context: web_sys::WebGl2RenderingContext = MagicBanner::context().unwrap();
-        let vert_shader = ShaderCompiler::vert_default(&context).unwrap();
-        let frag_shader = ShaderCompiler::frag_default(&context).unwrap();
-
-        let program = ProgramLinker::exec(&context, &vert_shader, &frag_shader)?;
-        context.use_program(Some(&program));
-
         let vertices: [f32; 9] = [-0.7, -0.7, 0.0, 0.7, -0.7, 0.0, 0.0, 0.7, 0.0];
 
-        let position_attribute_location = context.get_attrib_location(&program, "position");
-        let buffer = context.create_buffer().ok_or("Failed to create buffer")?;
-        context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&buffer));
-
-        // Note that `Float32Array::view` is somewhat dangerous (hence the
-        // `unsafe`!). This is creating a raw view into our module's
-        // `WebAssembly.Memory` buffer, but if we allocate more pages for ourself
-        // (aka do a memory allocation in Rust) it'll cause the buffer to change,
-        // causing the `Float32Array` to be invalid.
-        //
-        // As a result, after `Float32Array::view` we have to be very careful not to
-        // do any memory allocations before it's dropped.
-        unsafe {
-            let positions_array_buf_view = js_sys::Float32Array::view(&vertices);
-
-            context.buffer_data_with_array_buffer_view(
-                WebGl2RenderingContext::ARRAY_BUFFER,
-                &positions_array_buf_view,
-                WebGl2RenderingContext::STATIC_DRAW,
-            );
-        }
-
-        let vao = context
-            .create_vertex_array()
-            .ok_or("Could not create vertex array object")?;
-
-        context.bind_vertex_array(Some(&vao));
-        
-        context.vertex_attrib_pointer_with_i32(
-            position_attribute_location as u32,
-            3,
-            WebGl2RenderingContext::FLOAT,
-            false,
-            0,
-            0,
-        );
-        context.enable_vertex_attrib_array(position_attribute_location as u32);
-
-        context.bind_vertex_array(Some(&vao));
-
-        let vert_count = (vertices.len() / 3) as i32;
-        MagicBanner::draw(&context, vert_count);
-
-        Ok(())
+        MagicBanner::render(&vertices, &context)
     }
 }
 
@@ -110,6 +61,59 @@ impl MagicBanner {
         context.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
 
         context.draw_arrays(WebGl2RenderingContext::TRIANGLES, 0, vert_count);
+    }
+
+    fn render(vertices: &[f32; 9], context: &web_sys::WebGl2RenderingContext) ->  Result<(), JsValue>  {
+        let vert_shader = ShaderCompiler::vert_default(&context).unwrap();
+        let frag_shader = ShaderCompiler::frag_default(&context).unwrap();
+
+        let program = ProgramLinker::exec(&context, &vert_shader, &frag_shader)?;
+        context.use_program(Some(&program));
+
+        let position_attribute_location = context.get_attrib_location(&program, "position");
+        let buffer = context.create_buffer().ok_or("Failed to create buffer")?;
+        context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&buffer));
+
+        // Note that `Float32Array::view` is somewhat dangerous (hence the
+        // `unsafe`!). This is creating a raw view into our module's
+        // `WebAssembly.Memory` buffer, but if we allocate more pages for ourself
+        // (aka do a memory allocation in Rust) it'll cause the buffer to change,
+        // causing the `Float32Array` to be invalid.
+        //
+        // As a result, after `Float32Array::view` we have to be very careful not to
+        // do any memory allocations before it's dropped.
+        unsafe {
+            let positions_array_buf_view = js_sys::Float32Array::view(vertices);
+
+            context.buffer_data_with_array_buffer_view(
+                WebGl2RenderingContext::ARRAY_BUFFER,
+                &positions_array_buf_view,
+                WebGl2RenderingContext::STATIC_DRAW,
+            );
+        }
+
+        let vao = context
+            .create_vertex_array()
+            .ok_or("Could not create vertex array object")?;
+
+        context.bind_vertex_array(Some(&vao));
+        
+        context.vertex_attrib_pointer_with_i32(
+            position_attribute_location as u32,
+            3,
+            WebGl2RenderingContext::FLOAT,
+            false,
+            0,
+            0,
+        );
+        context.enable_vertex_attrib_array(position_attribute_location as u32);
+
+        context.bind_vertex_array(Some(&vao));
+
+        let vert_count = (vertices.len() / 3) as i32;
+        MagicBanner::draw(&context, vert_count);
+
+        Ok(())
     }
 }
 
