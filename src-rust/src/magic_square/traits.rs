93f5ab7c84d009c8695f519e370fbd03bd64fcef
nathanielBellamy <nbschieber@gmail.com>
2023-05-03 23:51:22 -0700

    set_next_slice

--- src-rust/src/magic_square/traits.rs
+++ src-rust/src/magic_square/traits.rs
@@ -12,15 +12,29 @@ pub trait VertexStore<T: Index<usize> + IndexMut<usize>> {
 
     fn set_idx(&mut self, new_index: usize) -> usize;
 
+    fn arr(&mut self) -> &mut [f32];
+
     fn set_next(&mut self, vertex: Vertex) {
         let idx: usize = self.idx();
         let arr: &mut [f32] = self.arr();
-        if self.idx() > arr.len() - 1 { return; }
+        if idx > arr.len() - 1 { return; }
         for i in 0..2 {
             arr[idx + i] = vertex[i]
         }
         self.set_idx(idx + 3);
     }
 
-    fn arr(&mut self) -> &mut [f32];
+    fn set_next_slice(&mut self, slice: &mut [f32]) {
+        let idx: usize = self.idx();
+        let arr: &mut [f32] = self.arr();
+        
+        let new_idx: usize = idx + slice.len();
+        if new_idx > arr.len() - 1 { return; }
+        
+        for (i, coord) in slice.iter().enumerate() {
+            arr[idx + i] = *coord;
+        }
+
+        self.set_idx(idx + slice.len());
+    }
 }
