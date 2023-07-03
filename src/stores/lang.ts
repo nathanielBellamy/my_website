11df5967be51d2795bc87b9ac14e8597c7aef460
nathanielBellamy <nbschieber@gmail.com>
2023-07-03 08:09:26 -0700

    fix type import

--- src/stores/lang.ts
+++ src/stores/lang.ts
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store'
 import type { Writable } from 'svelte/store'
-import { Lang } from '../I18n'
+import type { Lang } from '../I18n'
 
 export const lang: Writable<Lang | null> = writable(null)
