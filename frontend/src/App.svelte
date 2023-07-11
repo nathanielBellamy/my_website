3706e9cddc72903d1ee42d861410346fcd553b57
Nate Schieber <87251044+nathanielBellamy@users.noreply.github.com>
2023-07-10 23:22:02 -0700

    MW-23-site-section-dropdown (#24)
    * basic dropdown working, need to fix styling
    
    * dropdown_icon
    
    * dropdown styling
    
    * dropdown styling + personal dropdown
    
    * chevron and styling

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import { onDestroy, onMount } from 'svelte'
+  import { push } from "svelte-spa-router"
+  import { Button as DropdownButton, Dropdown, DropdownItem, Chevron } from 'flowbite-svelte'
   import Device from 'svelte-device-info'
   import Router from "svelte-spa-router"
   import {wrap} from 'svelte-spa-router/wrap'
@@ -7,7 +9,7 @@
   import Language from "./lib/Language.svelte"
   import { I18n, Lang } from "./I18n"
   import { lang } from "./stores/lang"
-  import { intoSiteSection, SiteSection, siteSection } from "./stores/siteSection"
+  import { intoSiteSection, intoUrl, SiteSection, siteSection } from "./stores/siteSection"
   import { touchScreen } from './stores/touchScreen'
 
   let touchScreenVal: boolean
@@ -51,22 +53,79 @@
     unsubTouchScreen()
   })
 
+  function handleDropdownClick(s: SiteSection) {
+    push(intoUrl(s))
+  }
 </script>
 
-<nav class="nav_bar flex items-center gap-2">
-  <!-- <div class="links flex justify-between items-stretch"> -->
-    <Link href="/" 
-          title={i18n.t("nav/home", langVal)}/> 
-    <Link href="/about" 
-          title={i18n.t("nav/about", langVal)}/>
-    <Link href="/magic_square" 
-          title={i18n.t("nav/magicSquare", langVal)}/>
-    <Link href="/give_me_a_sine" 
-          title={i18n.t("nav/giveMeASine", langVal)}/>
-  <!-- </div> -->
-  <!-- <div class="curr_section hidden md:block"> -->
-  <!--   {i18n.t(`nav/${siteSectionVal}`, langVal)} -->
-  <!-- </div> -->
+<nav class="nav_bar flex justify-between items-center gap-2 pt-2 pb-2">
+  <!-- <Dropdown> -->
+    <!-- <DropdownHeader> -->
+
+  <DropdownButton id="siteSectionDropdown"
+                  class="border-transparent"
+                  color="none"
+                  size='xs'>
+    <div class="dropdown_icon pb-1 flex justify-around items-center">
+      ☰
+    </div>
+  </DropdownButton>
+  <Dropdown triggeredBy="#siteSectionDropdown"
+            placement={touchScreenVal ? "bottom" : "right"}
+            ulClass="pt-2 pb-2 rounded-lg bg-zinc-800 text-blue-200">
+    <DropdownItem class="hover:bg-transparent w-11/12 flex items-center font-bold"
+                  on:click={() => handleDropdownClick(SiteSection.home)}>
+      {i18n.t("nav/home", langVal)} 
+    </DropdownItem>
+    <DropdownItem class="hover:bg-transparent w-11/12 flex items-center font-bold"
+                  on:click={() => handleDropdownClick(SiteSection.about)}>
+      {i18n.t("nav/about", langVal)}
+    </DropdownItem>
+    <DropdownItem class="hover:bg-transparent w-11/12 flex items-center font-bold"
+                  on:click={() => handleDropdownClick(SiteSection.magicSquare)}>
+      {i18n.t("nav/magicSquare", langVal)}
+    </DropdownItem>
+    <DropdownItem class="hover:bg-transparent w-11/12 flex items-center font-bold"
+                  on:click={() => handleDropdownClick(SiteSection.giveMeASine)}>
+      {i18n.t("nav/giveMeASine", langVal)}
+    </DropdownItem>
+  </Dropdown>
+
+  <DropdownButton id="contactInfo"
+                  class="border-transparent"
+                  color="none"
+                  size='xs'>
+    <div class="dropdown_personal flex justify-around items-center">
+      Nate Schieber
+      <div class="pb-1">
+        <Chevron />
+      </div>
+    </div>
+  </DropdownButton>
+  <Dropdown triggeredBy="#contactInfo"
+            placement="bottom"
+            ulClass="pt-3 pb-3 rounded-lg bg-zinc-800 text-blue-200">
+    <DropdownItem class="hover:bg-transparent w-11/12 flex items-center font-bold">
+      <Link href="mailto:nbschieber@gmail.com"
+            title="nbschieber@gmail.com"
+            sameOrigin={false}/>
+    </DropdownItem>
+    <DropdownItem class="hover:bg-transparent w-11/12 flex items-center font-bold">
+      <Link href="https://linkedin.com/in/nateschieber"
+            title="in/nateschieber"
+            sameOrigin={false}/>
+    </DropdownItem>
+    <DropdownItem class="hover:bg-transparent w-11/12 flex items-center font-bold">
+      <Link href="https://github.com/nathanielBellamy"
+            title="github.com/nathanielBellamy"
+            sameOrigin={false}/>
+    </DropdownItem>
+    <DropdownItem class="text-blue-200 hover:bg-transparent w-11/12 flex items-center font-bold">
+      <Link href="https://www.travelportland.com"
+            title="PORTLAND, OR"
+            sameOrigin={false}/>
+    </DropdownItem>
+  </Dropdown>
 </nav>
 
 <main class="rounded-md flex flex-col justify-start">
@@ -93,14 +152,24 @@
 <style lang="sass">
   @use "./styles/color"
   @use "./styles/text"
-  
+
+  .dropdown
+    &_icon
+      font-size: text.$fs-l
+      font-weight: text.$fw-l
+      color: color.$blue-4
+
+    &_personal
+      font-size: text.$fs-m
+      font-weight: text.$fw-l
+      color: color.$blue-4
+
   .nav_bar
     width: 100%
-    overflow-x: scroll
-  .curr_section
-    color: color.$blue-7
-    font-size: text.$fs-l
-    font-weight: text.$fw-l
+  /* .curr_section */
+  /*   color: color.$blue-7 */
+  /*   font-size: text.$fs-l */
+  /*   font-weight: text.$fw-l */
 
   .city
     font-weight: text.$fw-l
