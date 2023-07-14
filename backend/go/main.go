dd030e6a94f439d1d90107995a97d6a33efa2c97
nathanielBellamy <nbschieber@gmail.com>
2023-07-13 23:18:01 -0700

    scaffolding websockets

--- backend/go/main.go
+++ backend/go/main.go
@@ -4,13 +4,37 @@ import (
     "fmt"
     "log"
     "net/http"
+    "github.com/gobwas/ws"
+	  "github.com/gobwas/ws/wsutil"
 )
 
 func main() {
     fmt.Printf("Starting server on 8080")
-    fs := http.FileServer(http.Dir("./../../frontend/dist/"))
 
+    fs := http.FileServer(http.Dir("./../../frontend/dist/"))
     http.Handle("/", fs)
+
+    ws := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+      conn, _, _, err := ws.UpgradeHTTP(r, w)
+      if err != nil {
+        fmt.Printf("UpgradeHTTP Error")
+      }
+      go func() {
+        defer conn.Close()
+
+        for {
+          msg, op, err := wsutil.ReadClientData(conn)
+          if err != nil {
+            fmt.Printf("ReadClientData Error")
+          }
+          err = wsutil.WriteServerMessage(conn, op, msg)
+          if err != nil {
+            fmt.Printf("WriteServerMessage Error")
+          }
+        }
+      }()
+    })
+    http.Handle("/public-square", ws)
 
     if err := http.ListenAndServe(":8080", nil); err != nil {
         log.Fatal(err)
