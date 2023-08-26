1928fd06746fcdb9f1a368d9a989b6bc51aedcbc
nathanielBellamy <nbschieber@gmail.com>
2023-08-25 22:08:21 -0700

    crop image, resize

--- frontend/src/SystemDiagram.svelte
+++ frontend/src/SystemDiagram.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <body class="w-full h-full bg-black flex justify-around items-center">
-  <img class="h-11/12 w-11/12"
+  <img class="h-5/6"
        src={sd}
        alt="System Diagram"/>
 </body>
