c58efff07bda643a5e653eb206208d28531524dd
nathanielBellamy <nbschieber@gmail.com>
2023-08-29 08:44:22 -0700

    setting up recaptcha on frontend

--- frontend/src/lib/Recaptcha.svelte
+++ frontend/src/lib/Recaptcha.svelte
@@ -1,16 +1,69 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
   export let hasPassed: boolean = false
   var onloadCallback = function() {
     grecaptcha.render('html_recaptcha_element', {
       'sitekey' : import.meta.env.VITE_GRECAPTCHA_KEY
     });
   };
+
+  let error: string
+  let token: string
+
+  async function handleCaptchaCallback() {
+    await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            recaptchaToken: token
+        })
+    })
+
+    
+    // reset recaptcha for future requests
+    resetCaptcha()
+  }
+
+  function handleCaptchaError() {
+    error = 'Recaptcha error. Please reload the page'
+  }
+
+  function resetCaptcha() {
+    window.grecaptcha.reset()
+  }
+
+  const handleSubmit = () => {
+    // reset any errors
+    error = ''
+    
+    // tell recaptcha to process a request
+    window.grecaptcha.execute()
+  }
+
+  onMount(() => {
+    window.handleCaptchaCallback = handleCaptchaCallback;
+    window.handleCaptchaError = handleCaptchaError;
+    window.resetCaptcha = resetCaptcha;
+  })
 </script>
 
 <body>
   <!-- TODO -->
   <!--   - create Go endpoint and have this hit it -->
   <form action="?" method="POST">
+    {#if error}
+      <div>
+          <small class="text-yellow-300 font-bold">{error}</small>
+      </div>
+    {/if}
+    <div class="g-recaptcha"
+         data-sitekey={import.meta.env.VITE_RECAPTCHA_SITE_KEY}
+         data-callback="handleCaptchaCallback"
+         data-expired-callback="resetCaptcha"
+         data-error-callback="handleCaptchaError"
+         data-size="invisible"/>
     <div id="html_recaptcha_element"></div>
     <br>
     <input type="submit" value="Submit">
