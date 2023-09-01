181bbd49046315d98457648e47d9fd7ad89b5afd
nathanielBellamy <nbschieber@gmail.com>
2023-08-31 17:36:14 -0700

    avoid recaptcha libraries, make some good ol http calls ourselves

--- backend/go/auth/recaptcha.go
+++ backend/go/auth/recaptcha.go
@@ -1,12 +1,11 @@
 package auth
 
 import (
-	"context"
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
-
-	recaptcha "cloud.google.com/go/recaptchaenterprise/apiv1"
-	recaptchapb "google.golang.org/genproto/googleapis/cloud/recaptchaenterprise/v1"
 
 	"github.com/rs/zerolog"
 )
@@ -16,6 +15,16 @@ type RecaptchaData struct {
   recaptchaSiteKey string
   token string
   recaptchaAction string
+}
+
+type AssessmentBody struct {
+  event AssessmentEvent
+}
+
+type AssessmentEvent struct {
+  token string
+  siteKey string
+  expectedAction string
 }
 
 func ValidateRecaptcha(token string, recaptchaAction string, log *zerolog.Logger) {
@@ -30,72 +39,39 @@ func ValidateRecaptcha(token string, recaptchaAction string, log *zerolog.Logger
   CreateAssessment(rData, log)
 }
 
-/**
-* Create an assessment to analyze the risk of an UI action.
-*
-* @param projectID: GCloud Project ID
-* @param recaptchaSiteKey: reCAPTCHA key obtained by registering a domain/app to use recaptcha services.
-* @param token: The token obtained from the client on passing the recaptchaSiteKey.
-* @param recaptchaAction: Action name corresponding to the token.
-*/
 func CreateAssessment(rData RecaptchaData, log *zerolog.Logger) {
- // Create the recaptcha client.
- // TODO: To avoid memory issues, move this client generation outside
- // of this example, and cache it (recommended) or call client.close()
- // before exiting this method.
-  ctx := context.Background()
-  client, err := recaptcha.NewClient(ctx)
+  url := fmt.Sprintf(
+    "https://recaptchaenterprise.googleapis.com/v1/projects/%s/assessments?key=%s",
+    rData.projectID,
+    rData.recaptchaSiteKey,
+  )
+  assessmentEvent := AssessmentEvent {
+    token: rData.token,
+    siteKey: rData.recaptchaSiteKey,
+    expectedAction: rData.recaptchaAction,
+  }
+  assessmentBody := AssessmentBody { event: assessmentEvent }
+  jsonBody, err := json.Marshal(assessmentBody)
   if err != nil {
-    fmt.Printf("Error creating reCAPTCHA client\n")
-  }
-  defer client.Close()
-
-  // Set the properties of the event to be tracked.
-  event := &recaptchapb.Event{
-    Token: rData.token,
-    SiteKey: rData.recaptchaSiteKey,
+    log.Error().
+        Err(err).
+        Msg("Recaptcha Assessment Json")
   }
 
-  assessment := &recaptchapb.Assessment{
-    Event: event,
-  }
+  b := bytes.NewBuffer(jsonBody)
 
-  // Build the assessment request.
-  request := &recaptchapb.CreateAssessmentRequest{
-    Assessment: assessment,
-    Parent: fmt.Sprintf("projects/%s", rData.projectID),
-  }
-
-  response, err := client.CreateAssessment(
-    ctx,
-    request,
+  req, err := http.NewRequest(
+    http.MethodPost, 
+    url, 
+    b,
   )
 
+  res, err := http.DefaultClient.Do(req)
   if err != nil {
-    fmt.Printf("%v", err.Error())
+    log.Error().
+        Err(err).
+        Msg("Recaptcha Response")
   }
-
-  // Check if the token is valid.
-  if response.TokenProperties.Valid == false {
-    fmt.Printf("The CreateAssessment() call failed because the token"+
-    " was invalid for the following reasons: %v",
-    response.TokenProperties.InvalidReason)
-    return
-  }
-
-  // Check if the expected action was executed.
-  if response.TokenProperties.Action == rData.recaptchaAction {
-    log.Info().
-        Uint("score", response.RiskAnalysis.Score).
-        Msg("reCAPTCHA Response Risk Analysis")
-        
-    // for _,reason := range response.RiskAnalysis.Reasons {
-    //   fmt.Printf(reason.String()+"\n")
-    // }
-
-    return response.RiskAnalysis.Score > 50
-  }
-
-  log.Error().
-      Msg("reCAPTCHA tag action attribute does not match the expected action to score")
+  
+  fmt.Printf("Recaptcha Res: %v", res)
 }
