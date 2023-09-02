025425cc6fcafca1ade38d9b21df0a934bfabce1
nathanielBellamy <nbschieber@gmail.com>
2023-09-02 10:02:14 -0700

    conditionally place toast

--- frontend/src/lib/Toaster.svelte
+++ frontend/src/lib/Toaster.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { onDestroy } from 'svelte'
   import { Toast } from 'flowbite-svelte'
   import { fly } from 'svelte/transition'
   import type { ToastColor } from './Toasty'
@@ -7,6 +8,32 @@
   export let text: string
   export let open: boolean | null
 
+
+  import { smallScreen } from '../stores/smallScreen'
+  let smallScreenVal: boolean
+  const unsubSmallScreen = smallScreen.subscribe((val: boolean | null) => smallScreenVal = val)
+
+  enum Position {
+    tr = "top-right",
+    tl = "top-left",
+    br = "bottom-right",
+    bl = "bottom-left",
+    none = "none"
+  }
+
+  let position: Position = Position.tr
+
+  $: if (smallScreenVal) {
+    position = Position.tr
+  }
+
+  $: if (!smallScreenVal) {
+    position = Position.br
+  }
+
+  onDestroy(() => {
+    unsubSmallScreen()
+  })
 </script>
 
 {#if open === null}
@@ -14,7 +41,7 @@
          class="bg-slate-800 text-sky-600"
          transition={fly}
          params="{{x: 200}}"
-         position="top-left">
+         position={position}>
     <svelte:fragment slot="icon">
       <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
       <span class="sr-only">Check icon</span>
@@ -27,7 +54,7 @@
          class="bg-slate-800 text-sky-600"
          transition={fly}
          params="{{x: 200}}"
-         position="top-left">
+         position={position}>
     <svelte:fragment slot="icon">
       <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
       <span class="sr-only">Check icon</span>
