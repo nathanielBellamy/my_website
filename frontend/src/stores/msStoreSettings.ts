c3a70f33d64966abebfaba4b7a70039c0a583a30
nathanielBellamy <nbschieber@gmail.com>
2023-09-08 08:08:31 -0700

    clean up todo - went with store

--- frontend/src/stores/msStoreSettings.ts
+++ frontend/src/stores/msStoreSettings.ts
@@ -1,16 +1,6 @@
 import { writable } from 'svelte/store'
 import type { Writable } from 'svelte/store'
 
-// TODO:
-// - constrol modules subscribe to this store directly
-// - this means that these values will be remembered/shared between PS + MS
-// - at the moment, control modules are container-agnostic
-// - we should figure out how we want to track the container
-//  - pass down a prop into the control modules
-//  vs.
-//  - subscribe to a currentContainer store that flips in the onMount + onDestroy actions
-//  of the square containers
-
 // these settings are non-input settings
 // a.k.a. settings that do NOT appear in UiBuffer.settings in RustWasm
 export interface MsStoreSettings {
