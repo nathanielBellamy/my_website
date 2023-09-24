e66f83d0bac46ddc1ee8df3a177c3d5ff1bafa9d
nathanielBellamy <nbschieber@gmail.com>
2023-09-24 14:43:30 -0700

    cleanup cypress comments

--- spec/cypress/support/index.ts
+++ spec/cypress/support/index.ts
@@ -3,10 +3,6 @@ import { Mode } from './mode'
 declare global {
   namespace Cypress {
     interface Chainable {
-      /**
-       * Custom command to select DOM element by data-cy attribute.
-       * @example cy.dataCy('greeting')
-       */
       epilepsy_warning(accept: boolean): Chainable<null>
       visit_home(value: string): Chainable<null>
       wait_for_loading_screen(): Chainable<null>
