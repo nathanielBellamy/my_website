9d97e8ddadc4a83b54bccb565418afb5a8caba3c
nathanielBellamy <nbschieber@gmail.com>
2024-02-07 13:48:52 -0800

    building out years/months

--- frontend/src/MonthlyBudget/Calendar.svelte
+++ frontend/src/MonthlyBudget/Calendar.svelte
@@ -1,31 +1,59 @@
 <script lang="ts">
   import { type Day, Days } from './Days'
+  import { type Month, Months } from './Months'
 
   let currentDayIdx = 0
   $: currentDay = Days[currentDayIdx % 7]
+
+  let currentMonth: Month = contemporaneousMonthOnLoad()
+  let currentYear: number = new Date().getFullYear()
+
+
+  function contemporaneousMonthOnLoad(): Month {
+    const monthId = new Date().getMonth()
+    return Months[monthId]
+  }
+
+  function setCurrentMonth(e: any): void {
+    const newMonthId = e.target.value
+    currentMonth = Months[newMonthId]
+  }
+
+  function setCurrentYear(e: any): void {
+    currentYear = parseInt(e.target.value)
+  }
 </script>
 
 <div
   class="
     h-full w-full
-    grid grid-cols-2 grid-rows-1
+    grid grid-rows-1 grid-cols-2
   ">
   <div
     class="
       h-full
-      flex flex-col justify-between
+      grid grid-rows-2 grid-cols-1 calendar-grid
     ">
     <div class="
       flex justify-around
       w-full h-full
+      pt-2 pb-2
     ">
+      <h1
+        class="
+          flex flex-col justify-end
+          font-mono font-bold text-cyan-500
+        ">
+        Monthly Budget
+      </h1>
       <div
         class="
-          flex flex-col justify-around
+          flex flex-col justify-end
         ">
         <label
           class="
             font-bold
+            text-left
           "
           for='calendar-month-select'>
           Month
@@ -34,12 +62,22 @@
           id='calendar-month-select'
           aria-label="Month"
           title="Month"
+          value={currentMonth.id}
+          on:change={(e) => setCurrentMonth(e)}
           class="
             h-1/2
           ">
-          <option>
-            Jan
-          </option>
+          {#each Object.values(Months) as month}
+            <option
+              value={month.id}
+              class="
+                w-full
+                flex justify-between
+              ">
+              <p> {month.id + 1}. </p>
+              <p> {month.name} </p>
+            </option>
+          {/each}
         </select>
       </div>
       <div
@@ -56,23 +94,27 @@
         <select
           id='calendar-year-select'
           title="Year"
+          value={currentYear}
+          on:change={setCurrentYear}
           class="
             h-1/2
           ">
-          <option>
-            2020
-          </option>
+          {#each {length: 100} as _, year}
+            <option
+              value={year + 1990}>
+              {year + 1990}
+            </option>
+          {/each}
         </select>
       </div>
     </div>
-    <div 
+    <div
       class="
-        bg-green-700
         grow h-full w-min-1/5
         grid grid-cols-7 grid-rows-6
       ">
       {#each Object.values(Days) as day}
-        <div 
+        <div
           class="
             h-full font-bold
             flex flex-col justify-around
@@ -85,7 +127,7 @@
           class="
            p-2 m-2
            calendar-day
-          " 
+          "
           class:current-day={currentDayIdx === idx}
           class:weekday={[1,2,3,4,5].includes(idx % 7)}
           class:weekend={[0,6].includes(idx % 7)}
@@ -103,16 +145,28 @@
     ">
     <div
       class="
-        flex justify-around
+        grid grid-rows-1 grid-cols-2
       ">
-      <h1>
-        { currentDay.abbreviation_3 }
+      <h1
+        class="
+          grow
+          flex justify-around
+        ">
+        <p>{ currentDay.abbreviation_3 }</p>
+        <p>{ currentMonth.abbreviation_3}</p>
+        <p>{ currentYear }</p>
       </h1>
-      <div>
-        Starting balance:
-      </div>
-      <div>
-        Ending balance:
+      <div
+        class="
+          grid grid-rows-2 grid-cols-1
+          text-left
+        ">
+        <div>
+          Starting balance:
+        </div>
+        <div>
+          Ending balance:
+        </div>
       </div>
     </div>
     <div
@@ -159,15 +213,17 @@
       </button>
     </div>
   </div>
-
 </div>
 
 <style lang="sass">
   @use "./../styles/color"
 
+  .calendar-grid
+    grid-template-rows: 12% 88%
+
   .calendar-day:hover
     background: color.$blue-4
-  
+
   .current-day
     background: color.$red-4 !important
 
