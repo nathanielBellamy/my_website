81598aab83452f0284b24dfbeaa282eae08234d7
Nate Schieber <nbschieber@gmail.com>
2026-02-25 08:01:07 -0800

    focus nav

--- frontend/marketing/src/app/pages/focus/focus.component.ts
+++ frontend/marketing/src/app/pages/focus/focus.component.ts
@@ -11,8 +11,8 @@ import { PageComponent } from '../../components/page/page.component';
 })
 export class FocusComponent {
   readonly technologies = [
-    'LLMs', 'C++', 'TypeScript', 'Java', 'Scala', 'Go', 'Rust', 'Ruby',
-    'PostgreSQL', 'SQLite', 'NixOS', 'Qt', 'Angular', 'Vue', 'Spring',
-    'Rails', 'Akka', 'CAF', 'Vim'
+    'NixOS', 'Vim', 'LLMs', 'TypeScript', 'Java', 'Scala', 'Go', 'C++', 'Rust', 'Ruby',
+    'PostgreSQL', 'SQLite', 'Qt', 'Angular', 'Vue', 'Spring',
+    'Rails', 'Akka', 'CAF', ''
   ];
 }
